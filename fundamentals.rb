727fa43f7a666b0cb55bd9c454e5c5e690d1749c
JaymeeDEV <jamiekellybrown@gmail.com>
2020-01-26 22:23:53 +0000

    Palindrome function

--- fundamentals.rb
+++ fundamentals.rb
@@ -1,5 +1,6 @@
-# Create variable
+# Create variables
 p = "deified"
+s = "hello world"
 
 # Split the string into characters, reverse it, and join it back
 p == p.split("").reverse.join("")
@@ -8,3 +9,17 @@ p == p.split("").reverse.join("")
 # Ruby allows you to compare strings forwards and backwards without the split/join
 p == p.reverse
 # true
+
+# Same functionality written within a function
+def palindrome?(string)
+  string == string.reverse
+end
+
+# Call function with variable 'p'
+palindrome?(p)
+# true
+
+# Call function with variable 's'
+palindrome?(s)
+# false
+
