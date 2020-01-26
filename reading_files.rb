9ad6c2c3f79e159f36100e0fd9e2fa9caf82a5fe
JaymeeDEV <jamiekellybrown@gmail.com>
2020-01-26 23:29:21 +0000

    Readlines to eliminate split

--- reading_files.rb
+++ reading_files.rb
@@ -3,8 +3,11 @@
 # 'gem install mhartl_palindrome'
 require 'mhartl_palindrome' # Use palindrome detector gem
 
-text = File.read("phrases.txt") # Open phrases.txt for reading
-text.split("\n").each do |line| # Split content on newlines and iterate through the resulting array
+#-(old) text = File.read("phrases.txt") # Open phrases.txt for reading
+#-(old) text.split("\n").each do |line| # Split content on newlines and iterate through the resulting array
+
+lines = File.readlines("phrases.txt") # Readlines method does the same thing without needing the additional split
+lines.each do |line|
   if line.palindrome?
     puts "palindrome detected: #{line}" # Print any line that's a palindrome
   end
